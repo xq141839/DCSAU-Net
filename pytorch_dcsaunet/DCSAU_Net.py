b5eb86cb4c5ce7a801ac96d5cd05e69412029d13
Nana7mi <98734126+xq141839@users.noreply.github.com>
2022-08-25 12:33:17 +0800

    Add files via upload

---
+++ pytorch_dcsaunet/DCSAU_Net.py
@@ -0,0 +1,125 @@
+import torch.nn as nn
+import torch.nn.functional as F
+import torch
+from pytorch_dcsaunet.encoder import CSA
+
+csa_block = CSA()
+
+class DownAndUp(nn.Module):
+    def __init__(self,in_channels, out_channels):
+       super(DownAndUp, self).__init__()
+       temp = out_channels
+       self.conv1 = nn.Sequential(
+            nn.Conv2d(in_channels, temp, kernel_size=3, stride=1, padding=1),   
+            nn.BatchNorm2d(out_channels),
+            nn.ReLU(inplace=True),
+            nn.Conv2d(temp, out_channels, kernel_size=3, stride=1, padding=1),
+            nn.BatchNorm2d(out_channels),
+            nn.ReLU(inplace=True), 
+        )
+    def forward(self, x):
+     
+        return self.conv1(x)
+
+class Up(nn.Module):
+    """Upscaling"""
+
+    def __init__(self):
+        super().__init__()
+        
+        self.up = nn.Upsample(scale_factor=2, mode='bilinear', align_corners=True)
+
+    def forward(self, x1, x2):
+        x1 = self.up(x1)
+        # input is CHW
+        
+        diffY = x2.size()[2] - x1.size()[2]
+        diffX = x2.size()[3] - x1.size()[3]
+
+        x1 = F.pad(x1, [diffX // 2, diffX - diffX // 2,
+                        diffY // 2, diffY - diffY // 2])
+
+        x = torch.cat([x2, x1], dim=1)
+        return x
+
+    
+class PFC(nn.Module):
+    def __init__(self,channels, kernel_size=7):
+        super(PFC, self).__init__()
+        self.input_layer = nn.Sequential(
+                    nn.Conv2d(3, channels, kernel_size, padding= kernel_size // 2),
+                    nn.ReLU(inplace=True),
+                    nn.BatchNorm2d(channels))
+        self.depthwise = nn.Sequential(
+                    nn.Conv2d(channels, channels, kernel_size, groups=channels, padding= kernel_size // 2),
+                    nn.ReLU(inplace=True),
+                    nn.BatchNorm2d(channels))
+        self.pointwise = nn.Sequential(
+                    nn.Conv2d(channels, channels, kernel_size=1),
+                    nn.ReLU(inplace=True),
+                    nn.BatchNorm2d(channels))
+    def forward(self, x):
+        x = self.input_layer(x)
+        residual = x
+        x = self.depthwise(x)
+        x += residual
+        x = self.pointwise(x)
+        return x
+    
+
+# inherit nn.module
+class Model(nn.Module):
+    def __init__(self,img_channels=3, n_classes=1):
+       super(Model, self).__init__()
+       self.pfc = PFC(64)
+       self.img_channels = img_channels
+       self.n_classes = n_classes
+       self.maxpool = nn.MaxPool2d(kernel_size=2)
+       self.out_conv = nn.Conv2d(64, n_classes, kernel_size=1, stride=1, padding=0)
+       self.up_conv1 = Up()
+       self.up_conv2 = Up()
+       self.up_conv3 = Up()
+       self.up_conv4 = Up()
+       self.down1 = csa_block.layer1
+       self.down2 = csa_block.layer2
+       self.down3 = csa_block.layer3
+       self.down4 = csa_block.layer4
+       self.up1 = csa_block.layer5
+       self.up2 = csa_block.layer6
+       self.up3 = csa_block.layer7
+       self.up4 = csa_block.layer8
+
+       
+    def forward(self, x):
+  
+        x1 = self.pfc(x)
+        x2 = self.maxpool(x1)
+
+        x3 = self.down1(x2)    
+        x4 = self.maxpool(x3)
+        
+        x5 = self.down2(x4)     
+        x6 = self.maxpool(x5)
+        
+        x7 = self.down3(x6)    
+        x8 = self.maxpool(x7)
+        
+        x9 = self.down4(x8)
+
+        x10 = self.up_conv1(x9,x7)
+
+        x11 = self.up1(x10)
+   
+        x12 = self.up_conv2(x11,x5)    
+        x13 = self.up2(x12)
+       
+        x14 = self.up_conv3(x13,x3)   
+        x15 = self.up3(x14)
+        
+        x16 = self.up_conv4(x15,x1)
+        x17 = self.up4(x16)
+        
+        x18 = self.out_conv(x17)
+        
+        #x19 = torch.sigmoid(x18)
+        return x18
---
+++ pytorch_dcsaunet/resnet.py
@@ -0,0 +1,246 @@
+"""ResNet variants"""
+import math
+import torch
+import torch.nn as nn
+
+from pytorch_dcsaunet import splat
+
+__all__ = ['ResNet', 'Bottleneck']
+
+class DropBlock2D(object):
+    def __init__(self, *args, **kwargs):
+        raise NotImplementedError
+
+class GlobalAvgPool2d(nn.Module):
+    def __init__(self):
+        """Global average pooling over the input's spatial dimensions"""
+        super(GlobalAvgPool2d, self).__init__()
+
+    def forward(self, inputs):
+        return nn.functional.adaptive_avg_pool2d(inputs, 1).view(inputs.size(0), -1)
+
+class Bottleneck(nn.Module):
+    """ResNet Bottleneck
+    """
+    expansion = 4
+    def __init__(self, inplanes, planes, stride=1, downsample=None,
+                 radix=1, cardinality=1, bottleneck_width=64,
+                 avd=False, avd_first=False, dilation=1, is_first=False,
+                 rectified_conv=False, rectify_avg=False,
+                 norm_layer=None, dropblock_prob=0.0, last_gamma=False,number=1,custom=0):
+        super(Bottleneck, self).__init__()
+        group_width = int(planes * (bottleneck_width / 64.)) * cardinality
+        if custom != 0:
+            inplanes = custom
+        self.conv1 = nn.Conv2d(inplanes, group_width, kernel_size=1, bias=False)
+        self.bn1 = norm_layer(group_width)
+        self.dropblock_prob = dropblock_prob
+        self.radix = radix
+        self.avd = avd and (stride > 1 or is_first)
+        self.avd_first = avd_first
+
+        if self.avd:
+            self.avd_layer = nn.AvgPool2d(3, stride, padding=1)
+            stride = 1
+
+        if dropblock_prob > 0.0:
+            self.dropblock1 = DropBlock2D(dropblock_prob, 3)
+            if radix == 1:
+                self.dropblock2 = DropBlock2D(dropblock_prob, 3)
+            self.dropblock3 = DropBlock2D(dropblock_prob, 3)
+
+        if radix >= 1:
+            self.conv2 = splat.SplAtConv2d(
+                group_width, group_width, kernel_size=3,
+                stride=stride, padding=dilation,
+                dilation=dilation, groups=cardinality, bias=False,
+                radix=radix, rectify=rectified_conv,
+                rectify_avg=rectify_avg,
+                norm_layer=norm_layer,
+                dropblock_prob=dropblock_prob)
+        elif rectified_conv:
+            from rfconv import RFConv2d
+            self.conv2 = RFConv2d(
+                group_width, group_width, kernel_size=3, stride=stride,
+                padding=dilation, dilation=dilation,
+                groups=cardinality, bias=False,
+                average_mode=rectify_avg)
+            self.bn2 = norm_layer(group_width)
+        else:
+            self.conv2 = nn.Conv2d(
+                group_width, group_width, kernel_size=3, stride=stride,
+                padding=dilation, dilation=dilation,
+                groups=cardinality, bias=False)
+            self.bn2 = norm_layer(group_width)
+
+        self.conv3 = nn.Conv2d(
+            group_width, planes * 4, kernel_size=1, bias=False)
+        self.bn3 = norm_layer(planes*4)
+
+        if last_gamma:
+            from torch.nn.init import zeros_
+            zeros_(self.bn3.weight)
+        self.relu = nn.ReLU(inplace=True)
+        self.downsample = downsample
+        self.dilation = dilation
+        self.stride = stride
+        #self.dp = nn.Dropout(0.1)
+
+    def forward(self, x):
+        residual = x
+
+        out = self.conv1(x)
+        #out = self.dp(out)
+        out = self.bn1(out)
+        if self.dropblock_prob > 0.0:
+            out = self.dropblock1(out)
+        out = self.relu(out)
+
+        if self.avd and self.avd_first:
+            out = self.avd_layer(out)
+            
+        
+        
+        out = self.conv2(out)
+        #out = self.dp(out)
+        if self.radix == 0:
+            out = self.bn2(out)
+            if self.dropblock_prob > 0.0:
+                out = self.dropblock2(out)
+            out = self.relu(out)
+
+        if self.avd and not self.avd_first:
+            out = self.avd_layer(out)
+            
+        
+
+        out = self.conv3(out)
+       # out = self.dp(out)
+        out = self.bn3(out)
+        if self.dropblock_prob > 0.0:
+            out = self.dropblock3(out)
+
+        if self.downsample is not None:
+            residual = self.downsample(x)
+
+        out += residual
+        out = self.relu(out)
+
+        return out
+
+class ResNet(nn.Module):
+    def __init__(self, block, layers, radix=1, groups=1, bottleneck_width=64,
+                 num_classes=1000, dilated=False, dilation=2,
+                 deep_stem=False, stem_width=64, avg_down=False,
+                 rectified_conv=False, rectify_avg=False,
+                 avd=False, avd_first=False,
+                 final_drop=0.0, dropblock_prob=0,
+                 last_gamma=False, norm_layer=nn.BatchNorm2d):
+        self.cardinality = groups
+        self.bottleneck_width = bottleneck_width
+        self.inplanes = stem_width*2
+        self.avg_down = avg_down
+        self.last_gamma = last_gamma
+        # ResNeSt params
+        self.radix = radix
+        self.avd = avd
+        self.avd_first = avd_first
+        
+        super(ResNet, self).__init__()
+        self.rectified_conv = rectified_conv
+        self.rectify_avg = rectify_avg
+        if rectified_conv:
+            from rfconv import RFConv2d
+            conv_layer = RFConv2d
+        else:
+            conv_layer = nn.Conv2d
+        conv_kwargs = {'average_mode': rectify_avg} if rectified_conv else {}
+        ConvFea = [32,64,128,256,512]
+        self.layer1 = self._make_layer(block, ConvFea[0], layers[0], norm_layer=norm_layer, is_first=False)
+        
+        self.layer2 = self._make_layer(block, ConvFea[1], layers[1], stride=1, norm_layer=norm_layer)
+        
+            
+
+        self.layer3 = self._make_layer(block, ConvFea[2], layers[2], stride=1,
+                                           dilation=1, norm_layer=norm_layer,
+                                           dropblock_prob=dropblock_prob)
+        
+        self.layer4 = self._make_layer(block, ConvFea[2], layers[2], stride=1,
+                                           dilation=1, norm_layer=norm_layer,
+                                           dropblock_prob=dropblock_prob)
+           
+            
+        self.layer5 = self._make_layer(block, ConvFea[1], layers[0], stride=1,
+                                            dilation=2, norm_layer=norm_layer,
+                                            dropblock_prob=dropblock_prob,inchannel=1024)
+        self.layer6 = self._make_layer(block, ConvFea[0], layers[1], stride=1,
+                                            dilation=1, norm_layer=norm_layer,
+                                            dropblock_prob=dropblock_prob,inchannel=512)
+        self.layer7 = self._make_layer(block, ConvFea[0] // 2, layers[2], stride=1, norm_layer=norm_layer,inchannel=256)
+        self.layer8 = self._make_layer(block, ConvFea[0] // 2, layers[3], norm_layer=norm_layer, is_first=False,inchannel=128) 
+
+        for m in self.modules():
+            if isinstance(m, nn.Conv2d):
+                n = m.kernel_size[0] * m.kernel_size[1] * m.out_channels
+                m.weight.data.normal_(0, math.sqrt(2. / n))
+            elif isinstance(m, norm_layer):
+                m.weight.data.fill_(1)
+                m.bias.data.zero_()
+
+    def _make_layer(self, block, planes, blocks, stride=1, dilation=1, norm_layer=None,
+                    dropblock_prob=0.0, is_first=True,inchannel=0):
+        downsample = None
+        if stride != 1 or self.inplanes != planes * block.expansion or inchannel != 0:
+            if inchannel != 0:
+                self.inplanes = inchannel
+            down_layers = []
+            if self.avg_down:
+                if dilation == 1:
+                    down_layers.append(nn.AvgPool2d(kernel_size=stride, stride=stride,
+                                                    ceil_mode=True, count_include_pad=False))
+                else:
+                    down_layers.append(nn.AvgPool2d(kernel_size=1, stride=1,
+                                                    ceil_mode=True, count_include_pad=False))
+                down_layers.append(nn.Conv2d(self.inplanes, planes * block.expansion,
+                                             kernel_size=1, stride=1, bias=False))
+            else:
+                down_layers.append(nn.Conv2d(self.inplanes, planes * block.expansion,
+                                             kernel_size=1, stride=stride, bias=False))
+            down_layers.append(norm_layer(planes * block.expansion))
+            downsample = nn.Sequential(*down_layers)
+        
+        layers = []
+        if dilation == 1 or dilation == 2:
+            layers.append(block(self.inplanes, planes, stride, downsample=downsample,
+                                radix=self.radix, cardinality=self.cardinality,
+                                bottleneck_width=self.bottleneck_width,
+                                avd=self.avd, avd_first=self.avd_first,
+                                dilation=1, is_first=is_first, rectified_conv=self.rectified_conv,
+                                rectify_avg=self.rectify_avg,
+                                norm_layer=norm_layer, dropblock_prob=dropblock_prob,
+                                last_gamma=self.last_gamma,custom=inchannel))
+        elif dilation == 4:
+            layers.append(block(self.inplanes, planes, stride, downsample=downsample,
+                                radix=self.radix, cardinality=self.cardinality,
+                                bottleneck_width=self.bottleneck_width,
+                                avd=self.avd, avd_first=self.avd_first,
+                                dilation=2, is_first=is_first, rectified_conv=self.rectified_conv,
+                                rectify_avg=self.rectify_avg,
+                                norm_layer=norm_layer, dropblock_prob=dropblock_prob,
+                                last_gamma=self.last_gamma))
+        else:
+            raise RuntimeError("=> unknown dilation size: {}".format(dilation))
+
+        self.inplanes = planes * block.expansion
+        for i in range(1, blocks):
+            layers.append(block(self.inplanes, planes,
+                                radix=self.radix, cardinality=self.cardinality,
+                                bottleneck_width=self.bottleneck_width,
+                                avd=self.avd, avd_first=self.avd_first,
+                                dilation=dilation, rectified_conv=self.rectified_conv,
+                                rectify_avg=self.rectify_avg,
+                                norm_layer=norm_layer, dropblock_prob=dropblock_prob,
+                                last_gamma=self.last_gamma))
+
+        return nn.Sequential(*layers)
